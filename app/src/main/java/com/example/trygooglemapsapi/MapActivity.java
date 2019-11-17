b525db629db47c90e657f396cd706a6d338ae473
alenaxx <46314154+alenaxx@users.noreply.github.com>
2019-11-17 22:16:42 +0300

    Google Map Android project

---
+++ app/src/main/java/com/example/trygooglemapsapi/MapActivity.java
@@ -0,0 +1,45 @@
+package com.example.trygooglemapsapi;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.os.Bundle;
+
+
+
+
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity     {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+
+
+
+
+    }
+
+
+
+
+
+}
